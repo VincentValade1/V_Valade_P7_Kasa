b4a39e8a731da3cda252319c1b1723f066a776ee
VincentValade <vincent.valade@live.fr>
2023-03-22 10:37:15 +0100

    projet fini

--- src/components/LogementCard.jsx
+++ src/components/LogementCard.jsx
@@ -6,10 +6,12 @@ function LogementCard({ id, title, cover }) {
     return (
         // notre card est un lien (hook Link), menant vers la page logement avec dans l'url l'id du logement de la card cliquée.
         // la card contient une image (cover) et un titre (title).
-        <Link to={`/logement/${id}`} className="card-link" key={id}>
-            <img src={cover} alt='apercu du logement' />
-            <h2>{title}</h2>
-        </Link>
+        <li key={id} className="card-link">
+            <Link to={`/logement/${id}`} key={id}>
+                <img src={cover} alt='apercu du logement' />
+                <h2>{title}</h2>
+            </Link>
+        </li>
     )
 }
 
